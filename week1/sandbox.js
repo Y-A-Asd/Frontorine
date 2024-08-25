a008d37b635c4ae69eb54aa74b48212cb183a032
Yousof <yosofasady2@gmail.com>
2024-08-25 17:42:24 +0330

    week one

---
+++ week1/sandbox.js
@@ -0,0 +1,15 @@
+// q1
+// let first_name = prompt("Please enter your first name");
+// let last_name = prompt("Please enter your last name");
+// let age = prompt("Please enter your age");
+
+// console.log(first_name, last_name, age)
+
+// q3  ali -> ila 
+
+
+    let first_name = prompt("Please enter your first name");
+    let last_name = prompt("Please enter your last name");
+
+    console.log(first_name)
+    // `ali ${last_name}`
