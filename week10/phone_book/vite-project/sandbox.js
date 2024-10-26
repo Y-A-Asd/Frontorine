c70a10b156781ca9c68ca502ca77ccc884f121de
yousof <yosofasady2@gmail.com>
2024-10-26 21:09:17 +0330

    week10 added

---
+++ week10/phone_book/vite-project/sandbox.js
@@ -0,0 +1,119 @@
+const endpoint = `https://65a12bff600f49256fb112be.mockapi.io/g3/contact`
+
+function request(url, method, context=null){
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    const options = {
+        'method': method,
+        'headers': headers,
+        'body': context ? JSON.stringify(context) : null,
+    };
+
+    return fetch(url,options)
+    .then(response => {
+        if (response.ok) {
+            return response.json()
+    }
+    else{
+        console.log(response)
+        return response.status
+    }
+
+}
+    )
+}
+
+function init(){
+fetch(endpoint)
+.then(response => response.json())
+.then(data => render(data))
+}
+function render(contacts) {
+    const contactList = document.getElementById('contact-list')
+    contactList.innerHTML = ''
+    contacts.forEach(contact => {
+        const listItem = document.createElement('li')
+        const listItemData = document.createElement('p')
+
+        listItemData.textContent = `${contact.name} (${contact.phone_number})`
+        listItem.setAttribute('id',contact.id)
+        listItem.appendChild(listItemData)
+        contactList.appendChild(listItem)
+        listItemData.addEventListener('click',() => editcontact(contact.id))
+    });
+
+}
+
+function editcontact(contactId) {
+    const contactContainer = document.getElementById(`${contactId}`)
+
+    let contactForm = document.querySelector('#dynamic-contact-form')
+    console.log(contactForm)
+    if (!contactForm) {
+        contactFormTemplate = document.querySelector('#contact-form-template')
+        let contactForm = contactFormTemplate.cloneNode()
+        contactForm.id = 'dynamic-contact-form'
+    }
+    contactForm.classList.remove('hidden')
+    contactContainer.appendChild(contactForm)
+
+    contactForm.querySelector('#contact_id').value = contactId
+    contactContainer.scrollIntoView({behavior: 'smooth', block: 'center'})
+}
+
+function canceledit() {
+    const contactForm = document.querySelector('#dynamic-contact-form')
+    if (contactForm) {
+        contactForm.querySelector('#contact_id').value = ''
+        contactForm.classList.add('hidden')
+    }
+}
+
+function deletecontact() {
+    
+}
+
+
+document.getElementById('dynamic-contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let phone_number = document.getElementById('phone_number')
+    let name = document.getElementById('name')
+    let id = document.getElementById('contact_id')
+    console.log(phone_number.value,name.value, id.value)
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    const context = {
+        'phone_number': phone_number.value,
+        'name': name.value,
+    }
+
+    const options = {
+        method: 'PUT',
+        headers: headers,
+        body: context ? JSON.stringify(context) : null,
+    };
+
+    fetch(`${endpoint}/${id.value}`,options)
+    .then(response => {
+        if (response.ok) {
+            return response.json()
+    }
+    else{
+        console.log(response)
+        return response.status
+    }
+
+}
+    ).then(code=>{
+        init()
+        console.log(code)
+    })
+})
+
+
+init()
